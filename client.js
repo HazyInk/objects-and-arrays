119d3a7fa1cea33dd50288ed08920d1898ec6219
HazyInk <reillysheapaston@gmail.com>
2023-10-12 08:14:32 -0600

    added index.html and client.js

---
+++ client.js
@@ -0,0 +1,30 @@
+//Array
+
+
+  //TBD
+  document.querySelector("#first").innerHTML = ;
+  string = "How many oranges";
+  string.charAt(0); //Displays 'H'
+
+  //TBD
+  document.querySelector("#second").innerHTML = ;
+  string.charAt(15); //Displays 's'
+
+  //TBD
+  document.querySelector("#third").innerHTML = ;
+
+
+
+//Objects
+
+
+  //TBD
+  document.querySelector("#forth").innerHTML = ;
+  oranges.toString() //Displays 25
+
+  //TBD
+  document.querySelector("#fifth").innerHTML = ;
+  oranges.toExponential(); //Displays 2.5e+1
+
+  //TBD
+  document.querySelector("#sixth").innerHTML = ;

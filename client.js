d1ad6cdfd5b52ae303263c6110b8e439f4e22f10
HazyInk <reillysheapaston@gmail.com>
2023-10-13 10:05:51 -0600

    added js coding for objects and arrays

--- client.js
+++ client.js
@@ -1,25 +1,56 @@
 //Array
+  let gem1 = "Jade";
+  let gem2 = "Onyx";
+  let gem3 = "Malachite"; 
+  let gem4 = "ruby";
+  const gems = ["Jade", "Onyx", "Malachite"];
 
+//Add to an array using the .push()
+  gems.push("ruby", "Pearl");
+  document.querySelector("#push").innerHTML = gems[3];
+  console.log(gems[3]);
+// Result: [ 'Jade', 'Onyx', 'Malachite', 'ruby', 'Pearl' ] 
 
-  //TBD
-  document.querySelector("#first").innerHTML = ;
+//Remove from an array using the .pop()
+  gems.pop("Pearl");
+  document.querySelector("#pop").innerHTML = gems.pop("Pearl");
+// Result: [ 'Jade', 'Onyx', 'Malachite', 'ruby' ] (removed the pearl)
 
-  //TBD
-  document.querySelector("#second").innerHTML = ;
-
-  //TBD
-  document.querySelector("#third").innerHTML = ;
-
-
+//Verify if elements exist in an array with .includes()
+  gems.includes("Malachite");
+  document.querySelector("#includes").innerHTML = gems.includes("Malachite");
+// Result: true
 
 //Objects
+  const dragon = {
+    firstName:"Healsy", 
+    species:"Dragon", 
+    age:100, 
+    eyeColor:"sapphire"
+  };
+// Result: "firstName:'Healsy', species:'Dragon', age:100, eyeColor:'sapphire'
 
+  //Create a nested array from your object by using Object.entries()
+  Object.entries(dragon)
+  document.querySelector("#nested").innerHTML = dragon.firstName + "," + dragon.species + "," + dragon.age + "," + dragon.eyeColor;;
 
-  //TBD
-  document.querySelector("#forth").innerHTML = ;
+  //Create a new object with the new Object() syntax
+  const bird = new Object({
+    firstName:"Chip", 
+    species:"Parrot", 
+    age:10, 
+    eyeColor:"Lime"
+  });
+  //Output: firstName: 'Chip', species: 'Parrot', age: 10, eyeColor: 'Lime'
+  document.querySelector("#new").innerHTML = bird.firstName + "," + bird.species + "," + bird.age + "," + bird.eyeColor;
 
-  //TBD
-  document.querySelector("#fifth").innerHTML = ;
-
-  //TBD
-  document.querySelector("#sixth").innerHTML = ;
+  //Create and log an array of all the keys of your object with Object.keys()
+  document.querySelector("#keys").innerHTML = Object.keys(dragon);
+  dragon = {
+    firstName:"Healsy", 
+    species:"Dragon", 
+    age:100, 
+    eyeColor:"sapphire"
+  };
+  Object.keys(dragon)
+  // Result: 'firstName', 'species', 'age', 'eyeColor'
